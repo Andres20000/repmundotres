9d7dd388169f875bf16cc65faf2decd7099aaff9
Andres20000 <30440211+Andres20000@users.noreply.github.com>
2017-08-02 13:56:40 -0500

    final

--- mundotres/ViewController.swift
+++ mundotres/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     func test(){
         //Comentario al iniciar la funcion test
         //desde el compu
+        //desde el server
             
     }
 
